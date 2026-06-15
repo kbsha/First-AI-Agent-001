3c1fadb075a5a83adc5acac4ae6077a5f955241e
kbsha <kibrunpa1@gmail.com>
2026-06-15 14:44:05 +0200

    test

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from groq import Groq
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 
-#load_dotenv()
+load_dotenv()
 
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
-api_key = st.secrets.get("GROQ_API_KEY")
-
-client = Groq(api_key=api_key)
 
 st.set_page_config(
     page_title="KB AI Assistant",
